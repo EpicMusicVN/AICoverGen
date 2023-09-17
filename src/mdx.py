f97822ccaa6b1ac63e2e05a8e704a9c69091d54f
Khoa Tran <khoa.tran@musicmaster.io>
2023-09-17 06:32:52 +0000

    fix bug Failed to create CUDAExecutionProvider

--- src/mdx.py
+++ src/mdx.py
@@ -4,12 +4,12 @@ import os
 import queue
 import threading
 import warnings
+import torch
 
 import librosa
 import numpy as np
 import onnxruntime as ort
 import soundfile as sf
-import torch
 from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
